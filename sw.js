170ba39638079f0fe99da3aa6167dec68915414d
Elmatador512 <kouadiokouadioarsene512@gmail.com>
2026-08-01 09:09:37 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11472719
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+self.monetagDomainVerification = "5gvci.com";
