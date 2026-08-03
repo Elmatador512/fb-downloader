3ed4b3d5a88c00e1f8749136de4b7a1d03e1897b
Elmatador512 <kouadiokouadioarsene512@gmail.com>
2026-08-03 19:52:18 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,157 @@
+const urlInput = document.getElementById("url");
+const button = document.getElementById("downloadBtn");
+const loader = document.getElementById("loader");
+const result = document.getElementById("result");
+
+
+button.addEventListener("click", async function(){
+
+    const url = urlInput.value.trim();
+
+    result.innerHTML = "";
+
+
+    if(url === ""){
+
+        result.innerHTML = `
+        <p style="color:red;font-weight:bold;">
+        ❌ Colle un lien vidéo.
+        </p>`;
+
+        return;
+    }
+
+
+    if(!url.startsWith("http://") && !url.startsWith("https://")){
+
+        result.innerHTML = `
+        <p style="color:red;font-weight:bold;">
+        ❌ Le lien n'est pas valide.
+        </p>`;
+
+        return;
+
+    }
+
+
+    loader.style.display="block";
+    button.disabled=true;
+
+
+    try{
+
+
+        const response = await fetch(
+            "https://api.cobalt.tools/api/json",
+            {
+
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+                url:url
+            })
+
+            }
+        );
+
+
+        const data = await response.json();
+
+
+        let videoUrl = null;
+
+
+        if(data.url){
+
+            videoUrl=data.url;
+
+        }
+
+
+        else if(data.picker && data.picker.length > 0){
+
+            videoUrl=data.picker[0].url;
+
+        }
+
+
+
+        if(videoUrl){
+
+
+            result.innerHTML=`
+
+            <p style="color:green;font-weight:bold;">
+            ✅ Vidéo prête
+            </p>
+
+            <br>
+
+            <a href="${videoUrl}" 
+            target="_blank"
+            style="
+            color:#1877f2;
+            font-size:18px;
+            font-weight:bold;
+            text-decoration:none;
+            ">
+
+            📥 Télécharger la vidéo
+
+            </a>
+
+            `;
+
+
+        }
+
+
+        else{
+
+
+            result.innerHTML=`
+
+            <p style="color:red;font-weight:bold;">
+            ❌ Vidéo introuvable.
+            Vérifie que le lien est public.
+            </p>
+
+            `;
+
+        }
+
+
+
+    }
+
+
+    catch(error){
+
+
+        result.innerHTML=`
+
+        <p style="color:red;font-weight:bold;">
+        ❌ Erreur du serveur.
+        Réessaie plus tard.
+        </p>
+
+        `;
+
+
+        console.log(error);
+
+
+    }
+
+
+
+    loader.style.display="none";
+    button.disabled=false;
+
+
+
+});
